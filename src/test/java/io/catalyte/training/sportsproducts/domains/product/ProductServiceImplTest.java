c4c0b4575fd50609c43347ddde1e1ce0926b8fd8
Greg Fisher <gfisher@catalyte.io>
2021-05-10 16:53:53 +0000

    add example tests using Junit5

--- src/test/java/io/catalyte/training/sportsproducts/domains/product/ProductServiceImplTest.java
+++ src/test/java/io/catalyte/training/sportsproducts/domains/product/ProductServiceImplTest.java
@@ -1,12 +1,50 @@
 package io.catalyte.training.sportsproducts.domains.product;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+import io.catalyte.training.sportsproducts.exceptions.ResourceNotFound;
+import java.util.Optional;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
 
 /**
  * This is a placeholder for file structure and is intentionally left blank
  */
+@ContextConfiguration(classes = {ProductServiceImpl.class})
 @RunWith(MockitoJUnitRunner.class)
+@WebMvcTest(ProductServiceImpl.class)
 public class ProductServiceImplTest {
 
+  @InjectMocks
+  private ProductServiceImpl productServiceImpl;
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Mock
+  private ProductRepository productRepository;
+
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+    when(productRepository.findById(anyLong())).thenReturn(Optional.of(new Product()));
+  }
+
+  @Test
+  public void getProductByIdThrowsErrorWhenNotFound() {
+    when(productRepository.findById(anyLong())).thenReturn(Optional.empty());
+    assertThrows(ResourceNotFound.class, () -> productServiceImpl.getProductById(123L));
+  }
 }
