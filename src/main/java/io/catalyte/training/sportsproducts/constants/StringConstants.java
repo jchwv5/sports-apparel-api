113c86de40fc3613bacbe73eb466d4903fb36ad3
Amir Sharapov <asharapov@catalyte.io>
2021-07-13 10:18:44 -0500

    [feature] remove client id]

--- src/main/java/io/catalyte/training/sportsproducts/constants/StringConstants.java
+++ src/main/java/io/catalyte/training/sportsproducts/constants/StringConstants.java
@@ -9,6 +9,6 @@ public class StringConstants {
   public static final String SERVER_ERROR = "500 An unexpected error occurred.";
   public static final String SERVICE_UNAVAILABLE = "503 Service Unavailable";
 
-  // Google Client ID --> Change this as necessary
-  public static final String GOOGLE_CLIENT_ID = "912899852587-7996nh9mlpvpa2446q0il4f9hj5o492h.apps.googleusercontent.com";
+  // Google Client ID
+  public static final String GOOGLE_CLIENT_ID = "";
 }
