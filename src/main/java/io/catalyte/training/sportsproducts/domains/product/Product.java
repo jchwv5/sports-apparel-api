56525c418b9d9990ee00901aa51c2029006001a4
Andrew Bounitch <abounitch@catalyte.io>
2021-08-03 14:32:09 -0400

    reformat and cleanup code

--- src/main/java/io/catalyte/training/sportsproducts/domains/product/Product.java
+++ src/main/java/io/catalyte/training/sportsproducts/domains/product/Product.java
@@ -1,7 +1,6 @@
 package io.catalyte.training.sportsproducts.domains.product;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
