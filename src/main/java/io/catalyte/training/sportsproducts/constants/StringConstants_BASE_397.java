0d31cf6e9dd429c132522657d58bc242118abc1d
Ravinder <rgruddanti@catalyte.io>
2021-08-05 10:46:22 -0500

    try to fix conflicts

---
+++ src/main/java/io/catalyte/training/sportsproducts/constants/StringConstants_BASE_397.java
@@ -0,0 +1,11 @@
+package io.catalyte.training.sportsproducts.constants;
+
+public class StringConstants {
+
+  // Exceptions and Errors
+  public static final String NOT_FOUND = "404 Not Found";
+  public static final String BAD_REQUEST = "400 Bad Request";
+  public static final String CONFLICT = "409 Conflict";
+  public static final String SERVER_ERROR = "500 An unexpected error occurred.";
+  public static final String SERVICE_UNAVAILABLE = "503 Service Unavailable";
+}
