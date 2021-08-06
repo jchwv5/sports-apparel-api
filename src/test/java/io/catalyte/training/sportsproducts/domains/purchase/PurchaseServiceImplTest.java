050bb0b70850047cf1fc2753f51c1c15a44ba9d6
jhelton1985 <86621873+jhelton1985@users.noreply.github.com>
2021-08-06 11:17:31 -0600

    integrate tests from with existing tests from sprint1 branch

--- src/test/java/io/catalyte/training/sportsproducts/domains/purchase/PurchaseServiceImplTest.java
+++ src/test/java/io/catalyte/training/sportsproducts/domains/purchase/PurchaseServiceImplTest.java
@@ -2,20 +2,32 @@ package io.catalyte.training.sportsproducts.domains.purchase;
 
 import io.catalyte.training.sportsproducts.data.ProductFactory;
 import io.catalyte.training.sportsproducts.domains.product.Product;
+
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import io.catalyte.training.sportsproducts.exceptions.ResourceNotFound;
+import io.catalyte.training.sportsproducts.exceptions.ServerError;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.dao.DataAccessException;
 import org.springframework.web.server.ResponseStatusException;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class PurchaseServiceImplTest {
 
   private final PurchaseServiceImpl testPurchaseValidation = new PurchaseServiceImpl(null, null, null);
@@ -39,6 +51,54 @@ public class PurchaseServiceImplTest {
   LineItem lineItem4 = new LineItem(104L, purch3, prod4, 6);
 
   Set<LineItem> s1 = new HashSet<>();
+
+
+    @InjectMocks
+    private PurchaseServiceImpl mockPurchaseServiceImpl;
+
+    @Mock
+    private PurchaseRepository mockPurchaseRepository;
+
+    List purchaseList = new ArrayList<>();
+    Purchase p1 = new Purchase();
+    Purchase p2 = new Purchase();
+
+    @Before
+    public void setUp() throws Exception {
+      MockitoAnnotations.initMocks(this);
+
+      DeliveryAddress d1 = new DeliveryAddress();
+      d1.setFirstName("Jack");
+      d1.setLastName("Smith");
+      d1.setDeliveryStreet("123 South St");
+      d1.setDeliveryStreet2("Apt 1");
+      d1.setDeliveryCity("New York");
+      d1.setDeliveryState("NY");
+      d1.setDeliveryZip(12345);
+      BillingAddress b1 = new BillingAddress();
+      b1.setBillingStreet("123 South St");
+      b1.setBillingStreet2("Apt 1");
+      b1.setBillingCity("New York");
+      b1.setBillingState("NY");
+      b1.setBillingZip(12345);
+      b1.setEmail("Jack@gmail.com");
+      b1.setPhone("555-555-5555");
+      CreditCard c1 = new CreditCard();
+      c1.setCardNumber(1000000000000000L);
+      c1.setCvv(510);
+      c1.setExpiration("11/11/2011");
+      c1.setCardholder("Jack Smith");
+      p1.setId(1L);
+      p1.setProducts(null);
+      p1.setDeliveryAddress(d1);
+      p1.setBillingAddress(b1);
+      p1.setCreditCard(c1);
+      purchaseList.add(p1);
+
+      when(mockPurchaseRepository.findPurchasesByBillingAddressEmail(anyString())).thenReturn(purchaseList);
+
+    }
+
 
   @Test
   public void checkForInactiveProductsEmptyPurchase() {
@@ -319,5 +379,36 @@ public class PurchaseServiceImplTest {
     assertThat(e).hasMessage(
         "Transaction declined - Cvv must be 3 digits; Card is expired; Name field must not be empty");
   }
+
+  /**
+   * Test that findAllPurchasesByEmail is returning the list of associated emails
+   */
+  @Test
+  public void findPurchasesByEmailReturnsJack() {
+    ArrayList<Purchase> expected = new ArrayList<>();
+    expected.add(p1);
+    List<Purchase> result = mockPurchaseServiceImpl.findPurchasesByEmail(p1.getBillingAddress().getEmail());
+    Assert.assertEquals(expected, result);
+  }
+
+  /**
+   * Test that finaAllPurchasesByEmail returns a 404 error when given an empty string
+   */
+  @Test (expected = ResourceNotFound.class)
+  public void findPurchasesByEmailReturns404() {
+    mockPurchaseServiceImpl.findPurchasesByEmail("");
+  }
+
+  /**
+   * Test that findAllPurchasesByEmail returns a server error when catching a DataAccessException
+   */
+  @Test(expected = ServerError.class)
+  public void findPurchasesByEmailReturnsDataAccessException()  {
+    Mockito.when(mockPurchaseRepository.findPurchasesByBillingAddressEmail(anyString())).thenThrow(Mockito.mock(DataAccessException.class));
+    mockPurchaseServiceImpl.findPurchasesByEmail("joe@gmail.com");
+  }
+
+
+
 }
 
