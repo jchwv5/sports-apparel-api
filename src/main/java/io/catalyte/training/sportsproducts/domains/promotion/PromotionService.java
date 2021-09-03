2423af382338cb3757f221591e83a36c02a7aeae
hlin <hlin@catalyte.io>
2021-09-03 09:21:33 -0400

    Code completed and dev tested

---
+++ src/main/java/io/catalyte/training/sportsproducts/domains/promotion/PromotionService.java
@@ -0,0 +1,85 @@
+package io.catalyte.training.sportsproducts.domains.promotion;
+
+import io.catalyte.training.sportsproducts.exceptions.ResourceNotFound;
+import io.catalyte.training.sportsproducts.exceptions.ServerError;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
+
+
+@Service
+public class PromotionService {
+
+  private final Logger logger = LogManager.getLogger(PromotionService.class);
+
+  @Autowired
+  private final PromotionRepository promotionRepository;
+
+  public PromotionService(
+      PromotionRepository promotionRepository) {
+    this.promotionRepository = promotionRepository;
+  }
+
+
+  /**
+   * Adds a new promotion with unique promo code to the database.
+   *
+   * @param promotion - the promotion to add
+   * @return - the persisted promotion
+   */
+  public Promotion createPromotionByCode(@RequestBody Promotion promotion) {
+    boolean codeIsAlreadyInUse = false;
+    try {
+      if (promotion.getCode() != null && !promotion.getCode().equals("")) {
+        codeIsAlreadyInUse = promotionRepository.existsByCode(promotion.getCode());
+      }
+    } catch (DataAccessException e) {
+      logger.error(e.getMessage());
+    }
+    if (codeIsAlreadyInUse) {
+      logger.info(
+          "Add new promotion failed, code already exists in the database: " + promotion.getCode());
+      throw new ResponseStatusException(HttpStatus.CONFLICT, "Email already exists: " + promotion.getCode());
+    }
+    Promotion persistedPromotion = null;
+    try {
+      persistedPromotion = promotionRepository.save(promotion);
+    } catch (DataAccessException e) {
+      logger.error(e.getMessage());
+    }
+    return persistedPromotion;
+  }
+
+  /**
+   * Retrieves the promotion with the provided id from the database.
+   *
+   * @param id - the id of the promotion to retrieve
+   * @return - the promotion
+   */
+  public Promotion getPromotionById(Long id) {
+    Promotion promotion;
+    try {
+      promotion = promotionRepository.findById(id).orElse(null);
+    } catch (DataAccessException e) {
+      logger.error(e.getMessage());
+      throw new ServerError(e.getMessage());
+    }
+    if (promotion != null) {
+      return promotion;
+    } else {
+      logger.info("Get by id failed, id does not exist in the database: " + id);
+      throw new ResourceNotFound(
+          "Get by id failed. id " + id + " does not exist in the database: ");
+    }
+  }
+
+
+}
+
+
+
